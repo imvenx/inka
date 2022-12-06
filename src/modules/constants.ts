3a120f9e355e401d1d069b5d2934852d233aded4
imv <imvenx@proton.me>
2022-12-06 21:02:01 +0100

    add fill-opacity to allowedAttrs

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -2,5 +2,5 @@
 export const allowedEls = ['svg', 'rect', 'path', 'g', 'ellipse', 'circle', 'line', 'polygon',
     'text', 'tspan']
 export const allowedAttrs = ['fill', 'x', 'y', 'height', 'width', 'd', 'path', 'stroke',
-    'stroke-width', 'stroke-fill', 'style']
+    'stroke-width', 'stroke-fill', 'style', 'fill-opacity']
 
