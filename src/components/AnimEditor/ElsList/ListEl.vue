cc28d824ede2b8bb6128585623ad91e8df33aaa8
imv <imvenx@proton.me>
2022-12-08 19:23:27 +0100

    title on  hover instead of on start

--- src/components/AnimEditor/ElsList/ListEl.vue
+++ src/components/AnimEditor/ElsList/ListEl.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="list-el" @click="toggleCollapse()" :title="outerHtml">
+  <div class="list-el" @click="toggleCollapse()" ref="cont">
     <span v-for="depth in el.depth">&nbsp;</span>
     <span>{{ el.isUncollapsed ? '▲' : '▼' }}</span>
     <q-icon :name="getIcon(el.tagName ?? '')" :title="`<${el.tagName}>`" />
@@ -17,18 +17,17 @@
 
 <script lang="ts" setup>
 import { SvEl } from 'src/models/models';
-import { ConfigM } from 'src/modules/config_m';
 import { StorageM } from 'src/modules/storage_m';
 import { onMounted, ref } from 'vue';
 import ListAttr from './ListAttr.vue';
 
 const props = defineProps<{ el: SvEl }>()
 
-let outerHtml = ref<string>('')
-
+const cont = ref()
 onMounted(() => {
-  if (ConfigM.inDebugMode)
-    outerHtml.value = document.getElementById(props.el.id)?.outerHTML ?? ''
+  cont.value.addEventListener('mouseover', () => {
+    cont.value.title = document.getElementById(props.el.id)?.outerHTML
+  })
 })
 
 function getIcon(tagName: string) {
