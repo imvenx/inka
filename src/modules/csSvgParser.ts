94657734b6aae5f9f61ab7c532161f08085f249b
imv <imvenx@proton.me>
2022-12-18 20:18:31 +0100

    same

--- src/modules/csSvgParser.ts
+++ src/modules/csSvgParser.ts
@@ -1,9 +1,5 @@
-import { L } from "app/dist/electron/UnPackaged/assets/index.8edcfc3a"
 import { KeyVal } from "src/models/models"
-import { svgEl } from "./anim_m"
-import { SvElM } from "./svel_m"
-import { svgIO } from "./svgIO_m"
-import { performanceTest, roundToDecimals } from "./utils"
+import { roundToDecimals } from "./utils"
 
 export abstract class CsSvgParser {
     // static async cssStylesToSvgAttributes(_el: Element): Promise<Element> {
@@ -95,14 +91,34 @@ export abstract class CsSvgParser {
      * like fill, stroke etc, but not for x, y, width, etc
      */
     static async updateAttrsFromInkaToInkscape(el: SVGElement): Promise<void> {
+
+        if (el.tagName === 'svg') return
+
         let x = (el.style as any).x?.replace('px', '')
         if (x) el.setAttribute('x', x)
 
         let y = (el.style as any).y?.replace('px', '')
         if (y) el.setAttribute('y', y)
 
+        if (el.tagName === 'rect' || el.tagName === 'ellipse') {
+            let rx = (el.style as any).rx
+            if (rx) el.setAttribute('rx', rx)
+            let ry = (el.style as any).ry
+            if (ry) el.setAttribute('ry', ry)
+        }
+
         let width = el.style.width?.replace('px', '')
+        if (width) el.setAttribute('width', width)
+
         let height = el.style.height?.replace('px', '')
+        if (height) el.setAttribute('height', height)
+
+        let d = (el.style as any).d
+        if (d) {
+            d = d.replace('path("', '').replace('")', '');
+            el.setAttribute('d', d);
+            (el.style as any).d = ''
+        }
 
         let transform = el.style.transform
 
@@ -152,43 +168,41 @@ export abstract class CsSvgParser {
 
         // TODO: compare with last state, return if not modified, focus on hierarcy if modified
         // const lastEl = await SvElM.getSvElById(SvElM.lastSvEl.value, el.id)
-        // console.log(lastEl);
-
-
-        // if (el.tagName === 'g') return []
 
         const x = (el.style as any).x || el.getAttribute('x')
-        const y = (el.style as any).y || el.getAttribute('y')
-        let width = el.style.width || el.getAttribute('width')
-        let height = el.style.height || el.getAttribute('height')
-
-        if (x && y && width && height) {
-            // const bbox = (el as SVGGraphicsElement).getBBox()
-
-            // const oX = parseInt(x) + parseInt(width) / 2
-            // const oY = parseInt(y) + parseInt(height) / 2
-
-            // const oX = bbox.x + bbox.width / 2
-            // const oY = bbox.y + bbox.height / 2
-            // console.log(oX, oY, bbox)
-        }
-
         if (x) {
             el.setAttribute('x', x)
             attrs.push({ key: 'x', val: x })
         }
 
+        const y = (el.style as any).y || el.getAttribute('y')
         if (y) {
             el.setAttribute('y', y)
             attrs.push({ key: 'y', val: y })
         }
 
+        if (el.tagName === 'rect' || el.tagName === 'ellipse') {
+
+            const rx = (el.style as any).rx || el.getAttribute('rx')
+            if (rx) {
+                el.setAttribute('rx', rx)
+                attrs.push({ key: 'rx', val: rx })
+            }
+            const ry = (el.style as any).ry || el.getAttribute('ry')
+            if (ry) {
+                el.setAttribute('ry', ry)
+                attrs.push({ key: 'ry', val: ry })
+            }
+        }
+
+        let width = el.style.width || el.getAttribute('width')
         if (width) {
             width.includes('px') ? '' : width += 'px'
             el.setAttribute('width', width)
             attrs.push({ key: 'width', val: width })
         }
 
+        let height = el.style.height || el.getAttribute('height')
         if (height) {
             height.includes('px') ? '' : height += 'px'
             el.setAttribute('height', height)
@@ -199,13 +213,24 @@ export abstract class CsSvgParser {
         if (style) {
             const fill = style.fill
             if (fill) { attrs.push({ key: 'fill', val: fill }) }
+
+            const stroke = style.stroke
+            if (stroke) { attrs.push({ key: 'stroke', val: stroke }) }
+
+            const strokeWidth = style.strokeWidth
+            if (strokeWidth) { attrs.push({ key: 'strokeWidth', val: strokeWidth }) }
+
+            const strokeDasharray = style.strokeDasharray
+            if (strokeDasharray) { attrs.push({ key: 'strokeDasharray', val: strokeDasharray }) }
+
+            const strokeLinecap = style.strokeLinecap
+            if (strokeLinecap) { attrs.push({ key: 'strokeLinecap', val: strokeLinecap }) }
         }
 
-        // const transform = el.transform.baseVal[0]
         const transform = el.getAttribute('transform')
         if (!transform) {
             if (el.tagName === 'g') {
-                attrs.push({ key: 'transform', val: 'rotate(0)' })
+                attrs.push({ key: 'transform', val: 'none' })
             }
         }
         else {
@@ -213,17 +238,21 @@ export abstract class CsSvgParser {
             const m = baseVal.matrix
 
             if (transform.includes('translate')) {
+                const v = `translate(${m.e.toFixed(3) ?? 0}px, ${m.f.toFixed(3) ?? 0}px)`
                 attrs.push({
                     key: 'transform',
-                    val: `translate(${m.e.toFixed(0) ?? 0}px, ${m.f.toFixed(0) ?? 0}px)`
+                    val: v
                 })
+                el.style.transform = v
             }
 
             else if (transform.includes('rotate')) {
+                const v = `rotate(${roundToDecimals(1, baseVal.angle)}deg)`
                 attrs.push({
                     key: 'transform',
-                    val: `rotate(${roundToDecimals(1, baseVal.angle)}deg)`
+                    val: v
                 })
+                el.style.transform = v
             }
 
             // else if (baseVal && transform.includes('matrix')) {
@@ -239,7 +268,14 @@ export abstract class CsSvgParser {
             //         val: matrix
             //     })
             // }
+
         }
+
+        if (el.tagName === 'path') {
+            const d = `path('${el.getAttribute('d')}')`
+            attrs.push({ key: 'd', val: d })
+        }
+
 
         // names.forEach(name => {
         //     if (allowedAttrs.includes(name)) {
