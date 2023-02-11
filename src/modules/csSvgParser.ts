535110208fa4bf5043694af924a390faaa22c8c3
imv <imvenx@proton.me>
2023-02-11 10:04:41 +0100

    convert path to bezier first aproach, not working

--- src/modules/csSvgParser.ts
+++ src/modules/csSvgParser.ts
@@ -1,9 +1,11 @@
 import { KeyVal } from "src/models/models"
+import { pathDataPolyfill } from "src/polyfills/pathData"
 import { AnimM } from "./anim_m"
 import { KfsM } from "./kfs_m"
 import { SvElM } from "./svel_m"
 import { roundToDecimals } from "./utils"
 
+pathDataPolyfill()
 export abstract class CsSvgParser {
 
     private static lastValue: any = {}
@@ -182,7 +184,7 @@ export abstract class CsSvgParser {
 
             if (transform.includes('translate')) {
 
-                console.log(el.id, m.e.toFixed(3))
+                // console.log(el.id, m.e.toFixed(3))
 
                 const v = `translate(${m.e.toFixed(3) ?? 0}px, ${m.f.toFixed(3) ?? 0}px)`
                 attrs.push({
@@ -233,6 +235,7 @@ export abstract class CsSvgParser {
 
         const d = `path("${el.getAttribute('d')}")`
         if (el.tagName === 'path') {
+            const asd = this.convertToCubicBezier(el)
             attrs.push({ key: 'd', val: d })
         }
 
@@ -331,4 +334,177 @@ export abstract class CsSvgParser {
 
         return rgbValue;
     }
+
+    static convertToCubicBezier = function (path: any) {
+        var x, y, x1, y1, x2, y2,
+            x0, y0,
+            bezier = [],
+            segs = path.getPathData();
+
+        for (var i = 0, len = segs.length; i < len; ++i) {
+            var seg = segs[i], c = seg.type;
+            console.log(seg)
+
+            seg.x = seg.values[0]
+            seg.y = seg.values[1]
+            seg.x1 = seg.values[2]
+            seg.y1 = seg.values[3]
+            seg.x2 = seg.values[4]
+            seg.y2 = seg.values[5]
+
+            switch (c) {
+                // Move
+                case 'M':
+                    x = x1 = x2 = 0;
+                    y = y1 = y2 = 0;
+                    x0 = seg.x;
+                    y0 = seg.y;
+                    bezier.push({ x: 0, y: 0 });
+                    break;
+                case 'm':
+                    // Have not seen this in a path, yet
+                    break;
+                // Line segment
+                case 'L':
+                    bezier.push({ x: x, y: y });
+                    bezier.push({ x: (seg.x - x0), y: (seg.y - y0) });
+                    bezier.push({ x: (seg.x - x0), y: (seg.y - y0) });;
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                case 'l':
+                    bezier.push({ x: x, y: y });
+                    bezier.push({ x: (x + seg.x), y: (y + seg.y) });
+                    bezier.push({ x: (x + seg.x), y: (y + seg.y) });;
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                // Cubic
+                case 'C':
+                    bezier.push({ x: (seg.x1 - x0), y: (seg.y1 - y0) });
+                    bezier.push({ x: (seg.x2 - x0), y: (seg.y2 - y0) });
+                    bezier.push({ x: (seg.x - x0), y: (seg.y - y0) });
+
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                case 'c':
+                    bezier.push({ x: (x + seg.x1), y: (y + seg.y1) })
+                    bezier.push({ x: (x + seg.x2), y: (y + seg.y2) });
+                    bezier.push({ x: (x + seg.x), y: (y + seg.y) });;
+
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                // Smooth
+                case 'S':
+                    //@ts-ignore
+                    bezier.push({ x: ((x + (x - x2))), y: ((y + (y - y2))) });
+                    bezier.push({ x: (seg.x2 - x0), y: (seg.y2 - y0) });
+                    bezier.push({ x: (seg.x - x0), y: (seg.y - y0) });;
+
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                case 's':
+                    //@ts-ignore
+                    bezier.push({ x: (x + (x - x2)), y: (y + (y - y2)) });
+                    bezier.push({ x: (x + seg.x2), y: (y + seg.y2) });
+                    bezier.push({ x: (x + seg.x), y: (y + seg.y) });;
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                // Quadratic
+                case 'Q':
+                    break;
+                case 'q':
+                    break;
+
+                // Horizontal
+                case 'H':
+                    bezier.push({ x: x, y: y });
+                    bezier.push({ x: (seg.x - x0), y: y });
+                    bezier.push({ x: (seg.x - x0), y: y });
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                case 'h':
+                    bezier.push({ x: x, y: y });
+                    bezier.push({ x: (seg.x + x), y: y });
+                    bezier.push({ x: (seg.x + x), y: y });
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                // Vertical
+                case 'V':
+                    bezier.push({ x: x, y: y });
+                    bezier.push({ x: x, y: (seg.y - y0) });
+                    bezier.push({ x: x, y: (seg.y - y0) });
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                case 'v':
+                    bezier.push({ x: x, y: y });
+                    bezier.push({ x: x, y: (seg.y + y) });
+                    bezier.push({ x: x, y: (seg.y + y) });
+                    x = bezier[bezier.length - 1].x;
+                    y = bezier[bezier.length - 1].y;
+                    x1 = bezier[bezier.length - 3].x;
+                    y1 = bezier[bezier.length - 3].y;
+                    x2 = bezier[bezier.length - 2].x;
+                    y2 = bezier[bezier.length - 2].y;
+                    break;
+                // Close
+                case 'Z':
+                    break;
+                case 'z':
+                    break;
+
+
+                case 't':
+                case 'a':
+            }
+        }
+
+        console.log(bezier)
+
+        return bezier;
+    }
 }
+
